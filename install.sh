3c4e4bbd3d3866957837ea8a1ed37e898c65b75e
Bachynin Ivan <bachynin.i@gmail.com>
2018-12-08 16:56:12 +0200

    fix installation instruction

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+# install zsh
+sudo apt-get update && sudo apt-get -y install zsh
+
+# change shell to zsh
+chsh -s /bin/zsh
+
 # download
 git clone -b custom --recursive https://github.com/vanyakosmos/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
@@ -6,6 +12,3 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-
-# change shell
-chsh -s /bin/zsh
